ca063e32eefa0697ba21dcf8fb8f1b6ebed359ac
vinit <vinitkkulkarni@gmail.com>
2023-04-24 00:44:25 +0530

    Added:code to call push()

--- LinkedListMainMethod.java
+++ LinkedListMainMethod.java
@@ -5,5 +5,9 @@ import com.bridgelabz.testingforlistlinked.MyLinkedList;
 public class LinkedListMainMethod {
     public static void main(String[] args) {
         MyLinkedList<Integer> linkedList1 = new MyLinkedList<>();
+
+        linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
     }
 }
